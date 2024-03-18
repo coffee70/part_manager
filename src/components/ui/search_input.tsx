3498ac274156d382727030c66fcca41146ed7a14
Evan <ehaeick2@gmail.com>
2024-03-18 17:29:23 -0400

    search input ui component

--- src/components/ui/search_input.tsx
+++ src/components/ui/search_input.tsx
@@ -1,29 +1,53 @@
+'use client'
 import * as React from "react"
+import { Search, X } from "lucide-react"
 
-import { cn } from "@/lib/utils"
-import { Search } from "lucide-react"
+type Props = Partial<{
+  value: string | number | readonly string[];
+  onChange: React.ChangeEventHandler<HTMLInputElement>
+}>
 
-export interface SearchInputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> { }
+const SearchInput = (props: Props) => {
+  const { value, onChange } = props
 
-const SearchInput = React.forwardRef<HTMLInputElement, SearchInputProps>(
-  ({ className, ...props }, ref) => {
-    return (
-      <div className="flex items-center bg-foreground text-muted-foreground px-2">
-        <Search />
-        <input
-          type='text'
-          className={cn(
-            "flex h-10 w-full bg-transparent px-3 py-2 text-sm placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
-            className
-          )}
-          ref={ref}
-          {...props}
-        />
-      </div>
-    )
+  const [showClear, setShowClear] = React.useState<boolean>(false);
+
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  const handleClear = () => {
+    if (inputRef.current) {
+      inputRef.current.value = "";
+      inputRef.current.focus();
+      setShowClear(false);
+    }
   }
-)
+
+  React.useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.addEventListener('input', () => {
+        setShowClear(inputRef.current?.value !== "");
+      });
+    }
+  }, []);
+
+  return (
+    <div className="flex items-center bg-foreground text-muted-foreground px-2">
+      <Search />
+      <input
+        ref={inputRef}
+        type='text'
+        className="flex h-10 w-full bg-transparent px-3 py-2 text-sm placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
+        onChange={onChange}
+        value={value}
+      />
+      {showClear &&
+        <button onClick={handleClear}>
+          <X />
+        </button>
+      }
+    </div>
+  )
+}
 SearchInput.displayName = "Input"
 
 export { SearchInput }
