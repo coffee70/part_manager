cb8476a490d7e3525a659f0d5ed410bfea727f11
Evan <ehaeick2@gmail.com>
2024-03-18 17:46:43 -0400

    badge ui component

---
+++ src/components/ui/badge.tsx
@@ -0,0 +1,18 @@
+import * as React from "react"
+
+type Props = {
+  label: string;
+  color: string;
+}
+
+export default function Badge(props: Props) {
+  const { label, color } = props
+  return (
+    <div
+      className="inline-flex items-center rounded-sm px-2 text-xs text-white font-bold transition-colors"
+      style={{ backgroundColor: color }}
+    >{label.toUpperCase()}</div>
+  )
+}
+
+
