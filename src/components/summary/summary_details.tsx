c373eff15f1948cc1084d25e546c3ee2ccd24d99
Evan <ehaeick2@gmail.com>
2024-03-29 10:04:31 -0400

    click away listener for edit fields

--- src/components/summary/summary_details.tsx
+++ src/components/summary/summary_details.tsx
@@ -1,6 +1,6 @@
 import Summary from "./summary_base"
-import EditableInput from "../editable_fields/editable_input/editable_input"
 import { FocusProvider } from "./summary_details_context";
+import Detail from "./summary_detail"
 
 export type DetailT = {
     id: number;
@@ -19,13 +19,11 @@ export default function SummaryDetails({ details }: SummaryDetailsProps) {
             <div className='flex flex-col space-y-1 text-sm'>
                 <FocusProvider ids={ids}>
                     {details.map(detail => (
-                        <div key={detail.id} className="flex items-center justify-between space-x-2 min-w-80">
-                            <div className='text-muted-foreground text-nowrap'>{`${detail.key}:`}</div>
-                            <EditableInput detail={detail} />
-                        </div>
+                        <Detail key={detail.id} detail={detail} />
                     ))}
                 </FocusProvider>
             </div>
         </Summary>
     )
 }
+
