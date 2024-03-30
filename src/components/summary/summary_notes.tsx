c0f75163b71872ae5879554e1f7cbf353f8cef2f
Evan <ehaeick2@gmail.com>
2024-03-30 14:11:48 -0400

    remove editable_textarea

--- src/components/summary/summary_notes.tsx
+++ src/components/summary/summary_notes.tsx
@@ -1,10 +1,9 @@
 import Summary from './summary_base';
-import { Textarea } from '../ui/textarea';
 
 export default function SummaryNotes() {
     return (
         <Summary title="Notes">
-            <Textarea placeholder='Click to add notes' />
+            <></>
         </Summary>
     )
 }
