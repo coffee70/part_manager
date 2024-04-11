db30b236482e08554933edd1fc73f5210a69c1d5
Evan <ehaeick2@gmail.com>
2024-04-11 11:08:15 -0400

    summary title component

---
+++ src/components/summary/summary_title.tsx
@@ -0,0 +1,21 @@
+import { Breadcrumb } from "../ui/breadcrumb"
+
+type Props = {
+    title: string;
+    items: {
+        label: string
+        href?: string
+    }[];
+}
+
+export default function SummaryTitle({ title, items }: Props) {
+    return (
+        <div className="flex items-center space-x-4">
+            <div className="bg-foreground w-16 h-16"></div>
+            <div className="flex flex-col">
+                <Breadcrumb items={items} />
+                <span className="text-3xl text-text font-bold">{title}</span>
+            </div>
+        </div>
+    )
+}
