07b2520197cd2007c24fdbae5eb19424a0c317da
Evan <ehaeick2@gmail.com>
2024-04-17 07:24:21 -0400

    change border color

--- src/components/split_pane/split_pane.tsx
+++ src/components/split_pane/split_pane.tsx
@@ -6,7 +6,7 @@ export default function SplitPane({ rightPaneSlot, leftPaneSlot }: Props) {
     return (
         <div className="flex w-full h-screen">
             <div className="flex-1 p-8 overflow-y-auto">{leftPaneSlot}</div>
-            <div className="border border-muted-foreground"></div>
+            <div className="border border-foreground"></div>
             <div className="flex-1 p-8 overflow-y-auto">{rightPaneSlot}</div>
         </div>
     )
