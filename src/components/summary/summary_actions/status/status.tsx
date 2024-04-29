04ceb4b5834a42fc1975358f42e03541336abfa5
Evan <ehaeick2@gmail.com>
2024-04-29 19:04:58 -0400

    make status button a client component

--- src/components/summary/summary_actions/status/status.tsx
+++ src/components/summary/summary_actions/status/status.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuTrigger, DropdownMenuItem } from '@/components/ui/dropdown-menu'
 import { fetchStatusData } from '@/api/data'
