52794476c8e485dfba3ddaef227510cdf2b4bc97
Evan <ehaeick2@gmail.com>
2024-05-05 12:52:04 -0400

    stop avatar in people from shrinking

--- src/components/ui/people.tsx
+++ src/components/ui/people.tsx
@@ -9,12 +9,12 @@ type Props = {
 export default function People({ name, at }: Props) {
     return (
         <div className="flex items-center space-x-2 justify-end text-sm">
+            <CircleUserIcon className="shrink-0" strokeWidth={1} />
             {at ? (
                 <span>{`${formatDate(at)} by ${name}`}</span>
             ) : (
                 <span>{name}</span>
             )}
-            <CircleUserIcon strokeWidth={1} />
         </div>
     )
 }
