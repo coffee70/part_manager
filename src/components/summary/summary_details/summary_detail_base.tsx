2e16aa544804269da6b5b2d5710a7a087b5d8607
Evan <ehaeick2@gmail.com>
2024-05-29 14:15:15 -0400

    turn summary default base into a forward ref

--- src/components/summary/summary_details/summary_detail_base.tsx
+++ src/components/summary/summary_details/summary_detail_base.tsx
@@ -1,14 +1,19 @@
 'use client'
 import React from "react";
+import { cn } from "@/lib/utils";
 
 type Props = {
     children: React.ReactNode;
+    className?: string;
 }
 
-export default function DetailBase({ children }: Props) {
+const DetailBase = React.forwardRef<HTMLDivElement, Props>(({ children, className }, ref) => {
     return (
-        <div className="flex items-center justify-between space-x-2 min-w-80 min-h-9">
+        <div ref={ref} className={cn("flex items-center justify-between space-x-2 min-w-80 min-h-9", className)}>
             {children}
         </div>
     )
-}
+})
+DetailBase.displayName = "DetailBase";
+
+export default DetailBase;
