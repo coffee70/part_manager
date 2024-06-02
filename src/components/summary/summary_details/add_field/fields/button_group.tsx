92dcfd4e659cdf0cb0f612e1aafbeb7ebc129f51
Evan <ehaeick2@gmail.com>
2024-06-02 11:50:09 -0400

    expand binary choice to more than two and add stacked feature

---
+++ src/components/summary/summary_details/add_field/fields/button_group.tsx
@@ -0,0 +1,27 @@
+import { CircleCheckIcon, CircleIcon } from 'lucide-react';
+import { cn } from '@/lib/utils';
+
+type Props = {
+    value?: string;
+    onChange: (value: string) => void;
+    labels: string[]
+    stacked?: boolean;
+}
+
+export default function ButtonGroup({ value, onChange, labels, stacked }: Props) {
+    return (
+        <div className="flex justify-between space-x-2 min-h-12">
+            {labels.map(label => (
+                <div
+                    key={label}
+                    className={cn("flex-1 flex items-center justify-between px-4 border border-muted-foreground", stacked ? 'flex-col py-2 space-y-2' : '')}
+                    onClick={() => onChange(label)}
+                >
+                    <span className='text-sm'>{label}</span>
+                    {value === label && <CircleCheckIcon strokeWidth={1} size={stacked ? 24 : 30} />}
+                    {value !== label && <CircleIcon strokeWidth={1} size={stacked ? 24 : 30} />}
+                </div>
+            ))}
+        </div>
+    )
+}
