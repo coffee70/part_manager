fc30261d9d2e3c60ba978f6c91ad7af2ab2239e3
Evan <ehaeick2@gmail.com>
2024-06-06 17:57:20 -0400

    custom field input to take in value onChange props

--- src/components/fields/input.tsx
+++ src/components/fields/input.tsx
@@ -6,7 +6,6 @@ import { cn } from '@/lib/utils';
 
 function useInput() {
     const [focused, setFocused] = React.useState(false);
-    const [value, setValue] = React.useState('');
     const inputRef = React.useRef<HTMLInputElement>(null);
     const handleFocus = () => {
         inputRef.current?.focus();
@@ -16,15 +15,30 @@ function useInput() {
         inputRef.current?.blur();
         setFocused(false);
     }
-    return { focused, value, setValue, inputRef, handleFocus, handleBlur }
+    return { focused, inputRef, handleFocus, handleBlur }
 
 }
 
-export default function Input() {
-    const { focused, value, setValue, inputRef, handleFocus, handleBlur } = useInput();
+type Props = {
+    placeholder?: string;
+    type?: React.HTMLInputTypeAttribute;
+    value?: React.InputHTMLAttributes<React.HTMLInputTypeAttribute>['value'];
+    onChange?: React.ChangeEventHandler<HTMLInputElement>;
+}
+
+export default function Input({ placeholder, type, value, onChange }: Props) {
+    const { focused, inputRef, handleFocus, handleBlur } = useInput();
     return (
         <div className={cn('group flex items-center border border-transparent', focused ? 'border-border' : 'hover:border-border')}>
-            <BaseInput ref={inputRef} value={value} onChange={(e) => setValue(e.target.value)} onFocus={handleFocus} onBlur={handleBlur}/>
+            <BaseInput
+                ref={inputRef}
+                placeholder={placeholder}
+                type={type}
+                value={value}
+                onChange={onChange}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+            />
             {!focused && (
                 <button className='bg-foreground p-1 invisible group-hover:visible' onClick={handleFocus}>
                     <PencilIcon />
