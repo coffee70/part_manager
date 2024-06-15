6047fff4179ae90049955f441d224862bf5ef081
Evan <ehaeick2@gmail.com>
2024-06-15 09:10:36 -0400

    statuses and transitions added to api and db

---
+++ src/api/statuses/route.ts
@@ -0,0 +1,25 @@
+import prisma from "@/lib/prisma";
+import { response } from "@/api/helpers";
+import { HttpStatus } from '../helpers'; // Add this line
+
+export async function GET() {
+    try {
+        const statuses = await prisma.status.findMany();
+        if (!statuses) return response(HttpStatus.NOT_FOUND);
+        return response(HttpStatus.OK, statuses);
+    } catch {
+        return response(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
+
+export async function POST(request: Request) {
+    const body = await request.json();
+    try {
+        const status = await prisma.status.create({
+            data: body
+        });
+        return response(HttpStatus.CREATED, status);
+    } catch {
+        return response(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
