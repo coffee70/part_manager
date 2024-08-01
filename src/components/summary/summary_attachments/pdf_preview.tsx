fa2277cd6d5ac310bc59cf15f9f21103fe6be8ac
Evan <ehaeick2@gmail.com>
2024-08-01 17:50:41 -0400

    fix width on attachments

--- src/components/summary/summary_attachments/pdf_preview.tsx
+++ src/components/summary/summary_attachments/pdf_preview.tsx
@@ -18,15 +18,31 @@ type Props = {
     setOpen: (open: boolean) => void;
 }
 
+const SCALE = 0.25;
+const WIDTH_TOLERANCE = 2;
+
 export default function PDFPreview({ file, setOpen }: Props) {
     const [hovered, setHovered] = React.useState<boolean>(false)
+    const [width, setWidth] = React.useState<number>(0)
+
+    const onLoadSuccess = async (pdf: any) => {
+        const page = await pdf.getPage(1);
+        const viewport = page.getViewport({ scale: SCALE });
+        setWidth(viewport.width + WIDTH_TOLERANCE);
+    }
+
     return (
         <div
             onMouseEnter={() => setHovered(true)}
             onMouseLeave={() => setHovered(false)}
             onClick={() => setOpen(true)}
+            style={{ width }}
         >
-            <Document file={file.url} className='border border-foreground'>
+            <Document
+                file={file.url}
+                className='border border-foreground'
+                onLoadSuccess={onLoadSuccess}
+            >
                 <Thumbnail
                     scale={0.25}
                     pageNumber={1}
@@ -34,7 +50,7 @@ export default function PDFPreview({ file, setOpen }: Props) {
                 />
             </Document>
             <div className={cn('flex items-center justify-between bg-foreground', !hovered ? 'invisible' : '')}>
-                <div className='text-sm p-2'>{file.name}</div>
+                <div className='text-sm p-2 overflow-hidden whitespace-nowrap text-ellipsis'>{file.name}</div>
                 <Button variant='icon' disabled={!hovered} className='p-2'>
                     <Trash2Icon strokeWidth={1} size={20} />
                 </Button>
