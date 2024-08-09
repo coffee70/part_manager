91cd3e2a8b8cf94886303bca3239246254fab19b
Evan <ehaeick2@gmail.com>
2024-08-08 20:12:28 -0400

    use bg foreground instead of secondary

--- src/components/data_actions/data_action_button.tsx
+++ src/components/data_actions/data_action_button.tsx
@@ -12,7 +12,7 @@ export const DataAction = React.forwardRef<HTMLButtonElement, Props>(({ children
             {enabled && (
               <div className='absolute z-10 top-[-6px] right-[-6px] bg-primary text-white text-xs font-bold rounded-full h-4 w-4 flex items-center justify-center' />
             )}
-            <Button ref={ref} variant="icon" className="relative bg-secondary border border-border h-10 w-10" {...props}>
+            <Button ref={ref} variant="icon" className="relative bg-foreground border border-border h-10 w-10" {...props}>
                 {children}
             </Button>
         </div>
