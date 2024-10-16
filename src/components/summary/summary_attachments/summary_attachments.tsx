def87590a7015970ad929da3b7113bb723138fb3
Evan <ehaeick2@gmail.com>
2024-10-15 20:54:39 -0400

    add tooltip to summary attachment action

--- src/components/summary/summary_attachments/summary_attachments.tsx
+++ src/components/summary/summary_attachments/summary_attachments.tsx
@@ -19,7 +19,7 @@ export default function SummaryAttachments({ files }: Props) {
     }
 
     return (
-        <SummaryBase title="Attachments" onAdd={handleUpload}>
+        <SummaryBase title="Attachments" action={handleUpload} label='Add Attachment'>
             <div className="flex flex-wrap gap-x-8 gap-y-2">
                 {files.map((file, index) => (
                     <Attachment key={index} file={file} />
