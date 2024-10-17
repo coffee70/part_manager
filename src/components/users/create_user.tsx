5b45b1af44233a3367c7ad9b5295984b9d539026
Evan <ehaeick2@gmail.com>
2024-10-16 20:47:17 -0400

    create user form closes after save

--- src/components/users/create_user.tsx
+++ src/components/users/create_user.tsx
@@ -1,10 +1,15 @@
+'use client'
+import React from 'react';
 import { Button } from "../ui/button"
 import { PlusIcon } from 'lucide-react';
 import UserForm from "./user_form";
 
 export default function CreateUser() {
+
+    const [open, setOpen] = React.useState(false)
+
     return (
-        <UserForm>
+        <UserForm open={open} onOpenChange={setOpen}>
             <Button variant='secondary'>
                 <div className="flex items-center">
                     <PlusIcon size={20} className='pr-1' />
