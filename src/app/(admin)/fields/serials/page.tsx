2d35fb74508ddc728509d9c782cd9ec18b0a8200
Evan <ehaeick2@gmail.com>
2024-11-06 17:44:44 -0500

    add fields page for all models

---
+++ src/app/(admin)/fields/serials/page.tsx
@@ -0,0 +1,19 @@
+import { getSections } from "@/server/sections/get_sections";
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+import Sections from "@/components/fields/sections";
+import { sectionKeys } from "@/lib/query_keys";
+
+export default async function Page() {
+    const queryClient = new QueryClient();
+
+    await queryClient.prefetchQuery({
+        queryKey: sectionKeys.all('serials'),
+        queryFn: () => getSections({ collection: 'serials' }),
+    })
+
+    return (
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <Sections />
+        </HydrationBoundary>
+    )
+}
