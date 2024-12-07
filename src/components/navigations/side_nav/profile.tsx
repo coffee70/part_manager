fb4c66e4c7755e8057c388563b61f340f1b34af7
Evan <ehaeick2@gmail.com>
2024-12-07 11:31:05 -0500

    profile grabs user data

--- src/components/navigations/side_nav/profile.tsx
+++ src/components/navigations/side_nav/profile.tsx
@@ -1,7 +1,4 @@
-import {
-    CircleUserIcon,
-    MoreHorizontalIcon
-} from "lucide-react"
+import { CircleUserIcon } from "lucide-react"
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -12,23 +9,23 @@ import {
 } from "../../ui/dropdown-menu"
 import Logout from "@/components/auth/logout"
 import { More } from "@/components/ui/more"
+import { getCurrentSession } from "@/server/auth/get_current_session"
 
 
-export default function Profile() {
+export default async function Profile() {
+    const { user } = await getCurrentSession();
+
     return (
         <div className='flex justify-between space-x-1 m-2 bg-neutral-400/30 p-2'>
             <div className='flex space-x-2'>
                 <CircleUserIcon size={48} strokeWidth={1} />
                 <div className='flex flex-col'>
-                    <span className='text-lg'>Jane Doe</span>
-                    <span className='text-xs'>Ceramics Engineer</span>
+                    <span className='text-lg'>{user?.name}</span>
+                    <span className='text-xs'>{user?.title}</span>
                 </div>
             </div>
             <DropdownMenu>
                 <DropdownMenuTrigger asChild className='focus-visible:outline-none'>
-                    {/* <button className='flex flex-col'>
-                        <MoreHorizontalIcon size={20} />
-                    </button> */}
                     <More />
                 </DropdownMenuTrigger>
                 <DropdownMenuContent className='w-56'>
