aaa5b23c1acc41de998cc0304d80d7f150382aab
Evan <ehaeick2@gmail.com>
2025-01-04 13:56:40 -0500

    allow anywhere click on checkbox

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -11,8 +11,8 @@ export function Checkbox(props: Props) {
     const { label, description, value, onChange } = props;
 
     return (
-        <div className="flex">
-            <div onClick={() => onChange(!value)} className="flex items-center justify-center cursor-pointer pr-2">
+        <div className="flex cursor-pointer" onClick={() => onChange(!value)}>
+            <div className="flex items-center justify-center pr-2">
                 {value && <CircleCheckIcon className='text-white bg-black rounded-full' strokeWidth={1} size={30} />}
                 {!value && <CircleIcon strokeWidth={1} size={30} />}
             </div>
