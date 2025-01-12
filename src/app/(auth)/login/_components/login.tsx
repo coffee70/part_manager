6d8e9f50e4b1e87f1b17704f414f1c394a9e3576
Evan <ehaeick2@gmail.com>
2025-01-12 17:37:11 -0500

    server action error handling on login

--- src/app/(auth)/login/_components/login.tsx
+++ src/app/(auth)/login/_components/login.tsx
@@ -16,7 +16,7 @@ export default function Login() {
         password: "",
     });
 
-    const { mutate, isPending, isError, error } = useMutation({
+    const { mutate, isPending, data } = useMutation({
         mutationFn: login,
     })
 
@@ -30,10 +30,10 @@ export default function Login() {
             <div className="flex flex-col space-y-4 shadow-md rounded-md border border-gray-200 p-4 w-1/4">
                 <h1 className="font-bold text-xl">Log in</h1>
                 <form onSubmit={handleSubmit} className="flex flex-col w-full">
-                    {isError && <Alert variant='destructive'>
+                    {data?.success === false && <Alert variant='destructive'>
                         <AlertCircle className="h-4 w-4" />
                         <AlertTitle>Error</AlertTitle>
-                        <AlertDescription>{error.message}</AlertDescription>
+                        <AlertDescription>{data.error}</AlertDescription>
                     </Alert>}
                     <label className="text-sm" htmlFor="username">Username</label>
                     <Input
--- src/server/auth/login.ts
+++ src/server/auth/login.ts
@@ -1,7 +1,7 @@
 'use server'
 import { db } from "@/lib/db";
 import { validators } from "../validators/validators";
-import { UserDoc } from "@/types/collections";
+import { ServerActionState, UserDoc } from "@/types/collections";
 import { verify } from "@node-rs/argon2";
 import { createSession, generateSessionToken } from "@/lib/session";
 import { setSessionTokenCookie } from "@/lib/cookies";
@@ -12,7 +12,7 @@ type Input = {
     password: string;
 }
 
-export async function login(input: Input) {
+export async function login(input: Input): Promise<ServerActionState> {
     const { username, password } = validators.input<Input>(input);
 
     if (
@@ -21,10 +21,10 @@ export async function login(input: Input) {
         username.length > 31 ||
         !/^[a-z0-9_-]+$/.test(username)
     ) {
-        throw new Error("Invalid username");
+        return { success: false, error: "Invalid username" };
     }
     if (typeof password !== "string" || password.length < 6 || password.length > 255) {
-        throw new Error("Invalid password");
+        return { success: false, error: "Invalid password" };
     }
     
     const existingUser = await db.collection<UserDoc>("users").findOne({ username });
@@ -38,7 +38,7 @@ export async function login(input: Input) {
         // Since protecting against this is non-trivial,
         // it is crucial your implementation is protected against brute-force attacks with login throttling etc.
         // If usernames are public, you may outright tell the user that the username is invalid.
-        throw new Error("Incorrect username or password");
+        return { success: false, error: "Incorrect username or password" };
     }
 
     const validPassword = await verify(existingUser.password_hash, password, {
@@ -48,11 +48,11 @@ export async function login(input: Input) {
         parallelism: 1
     });
     if (!validPassword) {
-        throw new Error("Incorrect username or password");
+        return { success: false, error: "Incorrect username or password" };
     }
 
     const token = generateSessionToken();
     const session = await createSession(token, existingUser._id); 
     setSessionTokenCookie(token, session.expires_at);
     redirect("/");
-}
+}   
