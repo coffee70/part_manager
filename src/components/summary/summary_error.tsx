d3ed283d5a1b1d3e603de3e774efe96f624bfeee
Evan <ehaeick2@gmail.com>
2025-01-22 20:53:49 -0500

    update summary error to match field error

--- src/components/summary/summary_error.tsx
+++ src/components/summary/summary_error.tsx
@@ -1,12 +1,13 @@
-import { ArrowLeftIcon } from "lucide-react";
+import { SearchXIcon } from "lucide-react";
 
 export default function SummaryError() {
     return (
         <div className="flex flex-col items-center justify-center space-y-4 h-full">
             <div className="p-2 bg-black text-white rounded-md">
-            <ArrowLeftIcon size={36} />
+                <SearchXIcon size={36} />
             </div>
-            <p className="text-center">Select or create an item to view more information about it.</p>
+            <p className="text-center text-xl font-bold">No Instances Found</p>
+            <p className="text-center">Select or create an instance to view more information about it.</p>
         </div>
     )
 }
