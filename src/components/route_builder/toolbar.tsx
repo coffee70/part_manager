776340c53dbf51047e35f20b95f3c3bb11f29cef
Evan <ehaeick2@gmail.com>
2025-02-23 08:24:17 -0500

    add tooltip to toolbar buttons

--- src/components/route_builder/toolbar.tsx
+++ src/components/route_builder/toolbar.tsx
@@ -1,8 +1,9 @@
 'use client'
 import React from "react";
-import { PlusIcon, TrashIcon } from "lucide-react";
+import { ArrowUpFromDotIcon, PlusIcon, TrashIcon } from "lucide-react";
 import StepForm from "./step_form";
 import { useBuilderContext } from "./builder.context";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 
 export default function Toolbar() {
     const {
@@ -14,18 +15,52 @@ export default function Toolbar() {
 
     return (
         <div className="absolute bottom-4 right-4 flex gap-2">
-            <button
-                className="flex items-center justify-center bg-foreground shadow-md rounded-full h-10 w-10 disabled:opacity-50"
-                disabled={!isItemSelected()}
-                onClick={() => removeSelectedItem()}
-            >
-                <TrashIcon size={24} />
-            </button>
-            <StepForm>
-                <button className="flex items-center justify-center bg-foreground shadow-md rounded-full h-10 w-10">
-                    <PlusIcon size={24} />
-                </button>
-            </StepForm>
+            
+            <Tooltip>
+                <TooltipTrigger>
+                    <button
+                        className="flex items-center justify-center bg-foreground shadow-md rounded-full h-10 w-10 disabled:opacity-50"
+                        disabled={!isItemSelected()}
+                        onClick={() => removeSelectedItem()}
+                    >
+                        <TrashIcon size={24} />
+                    </button>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <div className="bg-black text-white text-xs px-2 py-1.5 rounded-md">
+                        <span>Delete</span>
+                    </div>
+                </TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+                <TooltipTrigger>
+                    <button className="flex items-center justify-center bg-foreground shadow-md rounded-full h-10 w-10">
+                        <ArrowUpFromDotIcon size={24} />
+                    </button>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <div className="bg-black text-white text-xs px-2 py-1.5 rounded-md">
+                        <span>Add Starting Step</span>
+                    </div>
+                </TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+                <TooltipTrigger>
+                    <StepForm>
+                        <button className="flex items-center justify-center bg-foreground shadow-md rounded-full h-10 w-10">
+                            <PlusIcon size={24} />
+                        </button>
+                    </StepForm>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <div className="bg-black text-white text-xs px-2 py-1.5 rounded-md">
+                        <span>Add Step</span>
+                    </div>
+                </TooltipContent>
+            </Tooltip>
+
             <button
                 className="flex items-center justify-center bg-primary text-white text-sm font-bold shadow-md rounded-full h-10 px-4 disabled:opacity-50"
                 disabled={!isEditing}
