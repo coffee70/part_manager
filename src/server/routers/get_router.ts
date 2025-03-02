ea52083d34137e15e32462f8691f50708b656c57
Evan <ehaeick2@gmail.com>
2025-03-02 08:55:35 -0500

    add server action to fetch individual router

---
+++ src/server/routers/get_router.ts
@@ -0,0 +1,29 @@
+'use server'
+import { db } from "@/lib/db"
+import { RouterDoc } from "@/types/collections"
+import { ObjectId } from "mongodb"
+import { z } from "zod"
+import { getCurrentSession } from "../auth/get_current_session"
+
+const InputSchema = z.object({
+    routerId: z.string().nullable().optional(),
+})
+
+export async function getRouter(input: z.input<typeof InputSchema>) {
+    const { user } = await getCurrentSession();
+    if (!user) throw new Error('Unauthorized');
+
+    const { routerId } = InputSchema.parse(input);
+
+    if (!routerId) return null;
+
+    const routerCollection = db.collection<RouterDoc>('routers');
+    const router = await routerCollection.findOne({ _id: new ObjectId(routerId) });
+
+    if (!router) throw new Error('Model not found');
+
+    return {
+        ...router,
+        _id: router._id.toString(),
+    };
+}
